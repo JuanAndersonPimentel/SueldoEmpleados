e3a3e43b56e3fdf2d71239b0b9158f8b42932f92
UserGPC <UserGPC@DESKTOP-4VVRAQ5>
2024-09-20 20:51:08 -0400

    Agregar archivos de proyecto.

---
+++ SueldoEmpleados/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SueldoEmpleados
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            int cantidadempleados = 0;
+            int empleadosSueldoBajo = 0;
+            int empleadosSueldoAlto = 0;
+            decimal totalSueldos = 0;
+            decimal sueldo = 0;
+
+            try
+            {
+                Console.WriteLine("ingrese la cantidad de empleados: ");
+                cantidadempleados = Convert.ToInt32(Console.ReadLine());
+
+                for (int i = 0; i < cantidadempleados; i++)
+                {
+                    Console.WriteLine($"ingrese el sueldo del empelado {i + 1}: ");
+                   decimal Sueldo = Convert.ToInt32(Console.ReadLine());
+                }
+                totalSueldos += sueldo;
+
+                if (sueldo <= 300)
+                {
+                    empleadosSueldoBajo++;
+                }
+                else
+                {
+                    empleadosSueldoAlto++;
+                }
+                Console.WriteLine($"empleados con sueldos entre 100 y 300: {empleadosSueldoBajo}");
+
+                Console.WriteLine($"empleados con sueldo mayor a 300: {empleadosSueldoAlto}");
+
+                Console.WriteLine($"total sueldos: ${totalSueldos}");
+            }
+            
+            catch (Exception)
+            {
+                Console.WriteLine("ocurrio un error:");
+                
+            }
+        }
+    }
+}
